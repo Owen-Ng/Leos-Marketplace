2123fd81584bc0eca4d9d66e82d4df08d46e5cdf
Owen Ng <s.nigelowennlc@gmail.com>
2022-04-28 16:24:20 -0400

    Setting up React

---
+++ marketplace/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar/Narvbar"
+function App() {
+  return (
+    <div className="min-h-screen">
+       <div className='gradient-bg-welcome'>
+         <Navbar/> 
+        </div>
+    </div>
+  );
+}
+
+export default App;
